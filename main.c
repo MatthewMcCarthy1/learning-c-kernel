4a20ae867296cda332e1388ab4a05d2580313911
Matthew <G00373167@atu.ie>
2026-02-14 14:23:43 +0000

    feat: implement pointer de-referencing to modify memory

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    printf("--- C Type Sizes on My Machine ---\n");
-    printf("char:      %zu byte(s)\n", sizeof(char));
-    printf("int:       %zu byte(s)\n", sizeof(int));
-    printf("long:      %zu byte(s)\n", sizeof(long));
-    printf("float:     %zu byte(s)\n", sizeof(float));
-    printf("double:    %zu byte(s)\n", sizeof(double));
-    printf("pointer:   %zu byte(s)\n", sizeof(void*));
-
     int my_data = 42;
     
-    // The '&' symbol means "Address of"
-    printf("The value is: %d\n", my_data);
-    printf("The memory address (location in RAM) is: %p\n", (void*)&my_data);
+    // 1. Create a pointer. 
+    // Think of this as a 'Remote Control' for the variable 'my_data'.
+    int *ptr = &my_data; 
+
+    printf("Before: %d\n", my_data);
+    printf("Pointer is looking at address: %p\n", (void*)ptr);
+
+    // 2. De-referencing (The '*' here means "Go to the address and act")
+    *ptr = 100; 
+
+    printf("After:  %d (Changed without touching the 'my_data' variable!)\n", my_data);
     
     return 0;
 }
